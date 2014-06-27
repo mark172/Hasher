083ba2b4d6a892cc85c10428f8d8134904e7b324
1lapilot <mark172@me.com>
2014-06-26 21:05:55 -0700

    initial commit

---
+++ Hasher/Hasher.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Hasher
+{
+    partial class frmHash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.txtHash = new System.Windows.Forms.TextBox();
+            this.btnHash = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "String";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hash";
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(52, 31);
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(471, 20);
+            this.txtText.TabIndex = 2;
+            // 
+            // txtHash
+            // 
+            this.txtHash.Location = new System.Drawing.Point(52, 72);
+            this.txtHash.Name = "txtHash";
+            this.txtHash.Size = new System.Drawing.Size(471, 20);
+            this.txtHash.TabIndex = 3;
+            // 
+            // btnHash
+            // 
+            this.btnHash.Location = new System.Drawing.Point(414, 125);
+            this.btnHash.Name = "btnHash";
+            this.btnHash.Size = new System.Drawing.Size(109, 29);
+            this.btnHash.TabIndex = 4;
+            this.btnHash.Text = "Hash";
+            this.btnHash.UseVisualStyleBackColor = true;
+            this.btnHash.Click += new System.EventHandler(this.btnHash_Click);
+            // 
+            // frmHash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 166);
+            this.Controls.Add(this.btnHash);
+            this.Controls.Add(this.txtHash);
+            this.Controls.Add(this.txtText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmHash";
+            this.Text = "Hasher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.TextBox txtHash;
+        private System.Windows.Forms.Button btnHash;
+    }
+}
+
---
+++ Hasher/Hasher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HashLibrary;
+
+namespace Hasher
+{
+    public partial class frmHash : Form
+    {
+        public frmHash()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHash_Click(object sender, EventArgs e)
+        {
+            txtHash.Text = HashLibrary.Hasher.HashString(txtText.Text.Trim());
+        }
+    }
+}
